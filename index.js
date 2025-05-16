dc007bc0c984bcc29416876b93fbc94998382295
SocialFunuruguay <j.ramirezvieira@gmail.com>
2025-05-16 15:03:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const db = require('./db');
 
 const express = require('express');
-const app = express();
+const app = express();:app.use(express.json()); // permite leer JSON en las peticiones
+
 const port = process.env.PORT || 3000;
 
 app.get('/ping', (req, res) => {
